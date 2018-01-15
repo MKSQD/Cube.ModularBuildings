f50a15b6c388cfde78d781e0e2212c60c76236fc
bioapfelsaft <bioapfelsaft@localhost>
2018-01-15 22:21:48 +0100

    item extension support (Builder.cs unfinished)

---
+++ BuilderTypeExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Core.Gameplay;
+
+namespace Core.ModularBuildings
+{
+    [CreateAssetMenu(menuName = "Core.Gameplay/BuilderItemTypeExtension")]
+    public class BuilderItemTypeExtension : ItemTypeExtension
+    {
+        public BuildingType buildingType;
+    }
+}
